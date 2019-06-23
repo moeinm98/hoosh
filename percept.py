4d08a9f79f2af9d2fe3d8d72bc9eebb7b2c3c4ce
sadegh khorasani <mohammadsakh@gmail.com>
2019-06-24 02:17:13 +0430

    reformat and add test

--- percept.py
+++ percept.py
@@ -1,42 +1,59 @@
-
 from utils import mnist_reader
 import numpy as np
 
 
-def perceptron_belong(img):
-    global w
-    MAX = -100000000000
-    I = 0
-    for i in range(len(w)):
-        if MAX < np.dot(img, w[i][1:]) + w[i][0]:
-            MAX = np.dot(img, w[i][1:]) + w[i][0]
-            I = i
-    return I
+class Perceptron:
+
+    def __init__(self, save_train_path, epoch, data_path):
+        w = [np.zeros(785) for i in range(10)]
+        w[0][0] = 1
+        self.w = w
+        self.save_train_path = save_train_path
+        self.data_path = data_path
+        self.epoch = epoch
+
+    def perceptron_belong(self, img):
+        MAX = -100000000000
+        I = 0
+        for i in range(len(self.w)):
+            if MAX < np.dot(img, self.w[i][1:]) + self.w[i][0]:
+                MAX = np.dot(img, self.w[i][1:]) + self.w[i][0]
+                I = i
+        return I
+
+    def perceptron_check(self, img, label):
+        guessed_label = self.perceptron_belong(img)
+        if guessed_label != label:
+            self.w[label][1:] = self.w[label][1:] + img
+            self.w[label][0] += 1
+            self.w[guessed_label][1:] = self.w[guessed_label][1:] - img
+            self.w[guessed_label][0] -= 1
+
+    def train(self):
+        images, labels = mnist_reader.load_mnist(self.data_path, kind='train')
+        for e in range(self.epoch):
+            for i in range(len(images)):
+                print(i)
+                self.perceptron_check(np.reshape(images[i], (784,)), labels[i])
+            print(self.w)
+
+    def test(self, test_data_path):
+        images, labels = mnist_reader.load_mnist(test_data_path, kind='t10k')
+        error = 0
+        for i in range(len(images)):
+            guessed_label = self.perceptron_belong(np.reshape(images[i], (784,)))
+            if guessed_label != labels[i]: error += 1
+        return 100-error / len(images) * 100
 
 
-def perceptron_check(img, label):
-    global w
-    guessed_label = perceptron_belong(img)
-    if guessed_label != label:
-        w[label][1:] = w[label][1:] + img
-        w[guessed_label][1:] = w[guessed_label][1:] - img
-
-
-images, labels = mnist_reader.load_mnist('data', kind='train')
-# X_test, y_test = mnist_reader.load_mnist('data', kind='t10k')
-
-print(((images[1])))
+# print(((images[1])))
 from PIL import Image
 
-w = [np.zeros(785) for i in range(10)]
-
-
-
-for i in range(len(images)):
-    print(i)
-    perceptron_check(np.reshape(images[i], (784,)), labels[i])
-
-
+perc = Perceptron('saved_path', 7, 'data')
+perc.train()
+ratio=perc.test('data')
+print(ratio)
+# print(w)
 # w, h = 512, 512
 # data = np.zeros((h, w, 3), dtype=np.uint8)
 # data[256, 256] = [255, 0, 0]
