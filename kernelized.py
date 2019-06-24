437db1e4f63c9eefb5d1348a6fb4ea8fefb8fede
sadegh khorasani <mohammadsakh@gmail.com>
2019-06-24 16:04:56 +0430

    revise kernelized to better performance

--- kernelized.py
+++ kernelized.py
@@ -4,19 +4,19 @@ import numpy as np
 
 class Kernelized:
     def __init__(self, save_train_path, epoch, data_path):
-        w = [np.zeros(60000) for i in range(10)]
-        self.w = w
+        self.w = [np.array([]) for i in range(10)]
+        self.w_index = [np.array([]) for i in range(10)]
         self.save_train_path = save_train_path
         self.data_path = data_path
         self.epoch = epoch
 
-    def perceptron_belong(self, img,s=60000):
+    def perceptron_belong(self, img):
         MAX = -100000000000
         I = 0
         for i in range(len(self.w)):
             sum = 0
-            for j in range(s):
-                sum += self.w[i][j] * self.K(self.images[j], img)
+            for j in range(len(self.w[i])):
+                sum += self.w[i][j] * self.K(self.images[int(self.w_index[i][j])], img)
             if MAX < sum:
                 MAX = sum
                 I = i
@@ -26,10 +26,12 @@ class Kernelized:
         return (np.dot(fxi, fx) + 2) ** 2
 
     def perceptron_check(self, img,i, label):
-        guessed_label = self.perceptron_belong(img,s=i)
+        guessed_label = self.perceptron_belong(img)
         if guessed_label != label:
-            self.w[label][i] += 1
-            self.w[guessed_label][i] -= 1
+            self.w[label]=np.append(self.w[label],[1])
+            self.w[guessed_label]=np.append(self.w[guessed_label],[-1])
+            self.w_index[label]=np.append(self.w_index[label],[i])
+            self.w_index[guessed_label]=np.append(self.w_index[guessed_label],[i])
 
     def train(self):
         self.images, labels = mnist_reader.load_mnist(self.data_path, kind='train')
@@ -53,9 +55,12 @@ class Kernelized:
 # print(((images[1])))
 from PIL import Image
 perc = Kernelized('saved_path', 1, 'data')
+
 perc.train()
-ratio = perc.test('data')
-print(ratio)
+# ratio = perc.test('data')
+print(perc.w)
+
+
 # print(w)
 # w, h = 512, 512
 # data = np.zeros((h, w, 3), dtype=np.uint8)
